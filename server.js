19b26064433894b8ef84664a80c1f10ff5eb908e
godanubex <louis.2542@hotmail.com>
2023-05-09 00:37:22 +0700

    db

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const multer = require("multer")
 const upload = multer({storage:multer.memoryStorage()});
 
 const db = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password: '1111',
   database: 'project'
